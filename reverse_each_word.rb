bd479b6b69cc4317fdc2a3e4c18b5526d11f82de
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-14 00:12:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,5 @@
 def reverse_each_word(string)
- 
- #array = string.split()
- 
- string.each{|word|word.reverse!}
- 
- return string
- 
- #return array.join(" ")
- 
-  
-  
+ array = string.split()
+ array.each{|word|word.reverse!}
+ return array.join("")
 end
